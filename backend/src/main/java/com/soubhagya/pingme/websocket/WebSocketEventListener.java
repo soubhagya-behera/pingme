7ace01a8081976b65f40cad9c5ba35ee6e3ce222
soubhagya-kumar-behera <soubhagyabehera074@gmail.com>
2026-07-01 01:32:46 +0530

    added backend of Online / Offline Presence

---
+++ backend/src/main/java/com/soubhagya/pingme/websocket/WebSocketEventListener.java
@@ -0,0 +1,84 @@
+package com.soubhagya.pingme.websocket;
+
+import com.soubhagya.pingme.entity.User;
+import com.soubhagya.pingme.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final UserRepository userRepository;
+
+    @EventListener
+    public void handleConnect(SessionConnectEvent event){
+
+        StompHeaderAccessor accessor =
+                StompHeaderAccessor.wrap(event.getMessage());
+
+        Object emailObj =
+                accessor.getSessionAttributes().get("email");
+
+        if(emailObj == null){
+
+            return;
+
+        }
+
+        String email = emailObj.toString();
+
+        User user =
+                userRepository.findByEmail(email)
+                        .orElse(null);
+
+        if(user != null){
+
+            user.setOnline(true);
+
+            userRepository.save(user);
+
+            System.out.println(user.getFullName() + " is ONLINE");
+
+        }
+
+    }
+
+    @EventListener
+    public void handleDisconnect(SessionDisconnectEvent event){
+
+        StompHeaderAccessor accessor =
+                StompHeaderAccessor.wrap(event.getMessage());
+
+        Object emailObj =
+                accessor.getSessionAttributes().get("email");
+
+        if(emailObj == null){
+
+            return;
+
+        }
+
+        String email = emailObj.toString();
+
+        User user =
+                userRepository.findByEmail(email)
+                        .orElse(null);
+
+        if(user != null){
+
+            user.setOnline(false);
+
+            userRepository.save(user);
+
+            System.out.println(user.getFullName() + " is OFFLINE");
+
+        }
+
+    }
+
+}
